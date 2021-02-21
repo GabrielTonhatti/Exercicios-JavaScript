de7bfbbc8682745dee5bcf3d99988739c010e6b7
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-02-21 15:56:26 -0300

    Exercicio 3 L2

--- Exercicio 3 L2/script3.js
+++ Exercicio 3 L2/script3.js
@@ -1,1 +1,12 @@
-console.log("hello")
+function calc() {
+    let firstnumber, secondnumber
+    firstnumber = Number(document.getElementById('firstnumber').value)
+    secondnumber = Number(document.getElementById('secondNumber').value)
+    
+    if (firstnumber < secondnumber){
+        document.getElementById('resultado').innerHTML = `O menor número é ${firstnumber}!`
+    }
+    else{
+        document.getElementById('resultado').innerHTML = `O menor número é ${secondnumber}!` 
+    }
+}
