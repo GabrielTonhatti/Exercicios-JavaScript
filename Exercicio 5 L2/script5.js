b2ebcbbdc3223a895f0b974b0f3a3d0e393d0a49
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-02-25 07:54:22 -0300

    Commit

--- Exercicio 5 L2/script5.js
+++ Exercicio 5 L2/script5.js
@@ -3,33 +3,34 @@ function calcula() {
     let num2 = Number(document.getElementById("num2").value)
     let opcao = Number(document.getElementById("opcao").value)
     let resultado
-    switch (opcao) {
+    switch (opcao){
         case 1:
             resultado = (num1 + num2) / 2
-            document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}`
-            break // sai do switch e não entra nos cases abaixo
+            document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}!`
+            break
         case 2:
             if (num1 >= num2) {
                 resultado = (num1 - num2)
-                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}`
+                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}!`
             } else {
                 resultado = (num2 - num1)
-                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}`
+                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}!`
             }
             break
         case 3:
             resultado = num1 * num2
-            document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}`
+            document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}!`
             break
         case 4:
             if (num2 != 0) {
                 resultado = num1 / num2
-                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}`
-            } else {
-                resultado = `Não é possível dividir por zero!`
+                document.getElementById("resultado").innerHTML = `O resultado da operação é ${resultado}!`
+            }
+            else{
+                document.getElementById("resultado").innerHTML = `Não é possível dividir por zero!`
             }
             break
         default:
-            resultado = `Opção Inválida! Tente novamente...`
+            document.getElementById("resultado").innerHTML = `Opção Inválida! Tente novamente...`
     }
 }
